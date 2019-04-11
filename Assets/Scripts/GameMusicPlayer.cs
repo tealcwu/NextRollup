2a294e4251f3c24cb11d53e461fed5a24273f754
Tealc Wu <tealcwu@hotmail.com>
2019-04-11 18:17:23 +0800

    add background music across scenes.

---
+++ Assets/Scripts/GameMusicPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicPlayer : MonoBehaviour {
+
+    private AudioSource _audioSource;
+
+    private static GameMusicPlayer instance = null;
+    
+    public static GameMusicPlayer Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
